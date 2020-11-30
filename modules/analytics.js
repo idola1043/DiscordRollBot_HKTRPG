5858973fb8894c9ef12d84a5496b273206bec1b6
idola1043 <satsujinki1983@gmail.com>
2020-12-01 02:06:42 +0800

    Update analytics.js

--- modules/analytics.js
+++ modules/analytics.js
@@ -83,10 +83,10 @@ function parseInput(inputStr) {
 	if (trigger.match(/choice|隨機|選項|選1/) != null && mainMsg.length >= 3) return exports.funny.choice(inputStr, mainMsg);
 
 	//tarot 指令
-	if (trigger.match(/tarot|塔羅牌|塔羅/) != null) {
-		if (trigger.match(/^單張|^每日|^daily/) != null) return exports.funny.NomalDrawTarot(mainMsg[1], mainMsg[2]); //預設抽 79 張
-		if (trigger.match(/^時間|^time/) != null) return exports.funny.MultiDrawTarot(mainMsg[1], mainMsg[2], 1);
-		if (trigger.match(/^大十字|^cross/) != null) return exports.funny.MultiDrawTarot(mainMsg[1], mainMsg[2], 2);
+	if (trigger.match(/-tarot|ainah/) != null) {
+		if (trigger.match(/^ehye-|^!daily/) != null) return exports.funny.NomalDrawTarot(mainMsg[1], mainMsg[2]); //預設抽 79 張
+		if (trigger.match(/^yar-|^!time/) != null) return exports.funny.MultiDrawTarot(mainMsg[1], mainMsg[2], 1);
+		if (trigger.match(/^throdog-|^!cross/) != null) return exports.funny.MultiDrawTarot(mainMsg[1], mainMsg[2], 2);
 	}
 
 	//FLAG指令開始於此
